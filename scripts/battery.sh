248220122344725e0c4fa728d5923cb66794f2fa
emily <git@emily.ly>
2023-06-08 11:07:43 +0100

    fix indentation

--- scripts/battery.sh
+++ scripts/battery.sh
@@ -1,53 +1,53 @@
 #!/usr/bin/env bash
 
 battery_status() {
-	case "$(uname -s)" in
-		Linux)
-			_bat=$(find /sys/class/power_supply -type l -name "BAT*")
+    case "$(uname -s)" in
+        Linux)
+            _bat=$(find /sys/class/power_supply -type l -name "BAT*")
             _status="$(cat "${_bat}/status")%"
-			;;
-		Darwin)
+            ;;
+        Darwin)
             _status="$(pmset -g batt | sed -n 2p | cut -d ';' -f 2 | tr -d ' ')"
-			;;
-		*)
-			;;
-	esac
+            ;;
+        *)
+            ;;
+    esac
 
-	case $_status in
-		discharging|Discharging)
-			echo '<'
-			;;
-		high|Full)
-			echo '♥ '
-			;;
-		charging|Charging)
-			echo '>'
-			;;
-		*)
-			echo ''
-			;;
-	esac
+    case $_status in
+        discharging|Discharging)
+            echo '<'
+            ;;
+        high|Full)
+            echo '♥ '
+            ;;
+        charging|Charging)
+            echo '>'
+            ;;
+        *)
+            echo ''
+            ;;
+    esac
 }
 
 battery_percent() {
-	case "$(uname -s)" in
-		Linux)
-			_bat=$(find /sys/class/power_supply -type l -name "BAT*")
+    case "$(uname -s)" in
+        Linux)
+            _bat=$(find /sys/class/power_supply -type l -name "BAT*")
             echo "$(cat "${_bat}/capacity")%"
-			;;
-		Darwin)
-			pmset -g batt | grep -Eo '[0-9]?[0-9]?[0-9]%'
-			;;
-		*)
-			;;
-	esac
+            ;;
+        Darwin)
+            pmset -g batt | grep -Eo '[0-9]?[0-9]?[0-9]%'
+            ;;
+        *)
+            ;;
+    esac
 }
 
 main() {
-	status="$(battery_status)"
-	percent="$(battery_percent)"
-	
-	echo "${status}${percent}"
+    status="$(battery_status)"
+    percent="$(battery_percent)"
+    
+    echo "${status}${percent}"
 }
 
 main
